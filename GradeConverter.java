8d825e2dbd549ffaaf8a477051259b8a1ec6201b
Isaiah Magana <Maganaisaiah4@gmail.com>
2022-03-28 13:43:14 -0400

    Created the base outline for the project

---
+++ GradeConverter.java
@@ -0,0 +1,17 @@
+/**
+ First pass through of the grade converter program which takes in a double score, and outputs a letter grade
+ @author (Isaiah Magana)
+ 
+ GradeConverter.java
+*/
+
+import java.util.Scanner;
+
+public class GradeConverter {
+	/**
+	main method to run program
+	*/
+	public static void main(String[] args) {
+		
+	}
+}
